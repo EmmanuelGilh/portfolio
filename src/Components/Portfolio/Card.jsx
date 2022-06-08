1ec7bf53227745e35edb48336251fd505732e4ef
unknown <egil90@hotmail.com>
2022-06-07 23:10:52 -0500

    projects halfway func, mejoras styles, bugs delete

--- src/Components/Portfolio/Card.jsx
+++ src/Components/Portfolio/Card.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
+import poke from '../../Media/poke.JPG'
+import mobi from '../../Media/mobi.JPG'
+import piedra from '../../Media/piedra.JPG'
+import './Card.css'
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 const Card = () => {
+
+
+
+
     return (
-        <div>
-            <div className="container">
-                {/* <div>
-                    <h1>titulo</h1>
-                    <h1>imagen</h1>
+        <div className="containerCard">
+            <div className="wrapperPort">
+
+                <div className='cards'>
+                    <img src={mobi} alt='mobi' className='imgCard' />
+                    <a href='https://mobi-app.netlify.app/' target='_blank'>
+                        <button > Mobi ATR</button>
+                    </a>
                 </div>
-                <div>
-                    <h1>titulo</h1>
-                    <h1>imagen</h1>
+
+                <div className='cards'>
+                    <img src={poke} alt='mobi' className='imgCard' />
+                    <button> Pokedex </button>
                 </div>
-                <div>
-                    <h1>titulo</h1>
-                    <h1>imagen</h1>
-                </div> */}
+
+                <div className='cards'>
+                    <img src={piedra} alt='Piedra, papel o tijera' className='imgCard' />
+                    <button>Piedra, papel o tijera</button>
+                </div>
+
             </div>
         </div>
     )
